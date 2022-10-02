c37447df4c1bd4af7204970c412cf53300e8f3f1
Alenochka <alen4k@gmail.com>
2022-10-02 04:12:15 +0300

    commits fixing

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,6 +3,7 @@ import React from 'react';
 
 function Footer() {
   const currentYear = new Date().getFullYear();
+  
   return (
     <footer className="footer">
       <p className="footer__copyright">&copy; {currentYear}. AlenaAlena &#38; Mesto</p>
